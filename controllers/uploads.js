ce432f779f8348baecd6dd40266d04bfb71be0d3
jonathan <jonathan@cbloom.pro>
2022-12-30 02:48:34 -0500

    adding cloudinary integration for files

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -2,6 +2,8 @@ const { request, response } = require("express")
 const path = require("path")
 const fs = require("fs")
 
+const cloudinary = require('cloudinary').v2;
+cloudinary.config(process.env.CLOUDINARY_URL)
 
 const { subirArchivo } = require("../helpers")
 const { Usuario, Producto } = require("../models")
@@ -50,23 +52,21 @@ const actualizarImagen = async (req = request, res = response) => {
             return res.status(500).json({ msg: 'ocurrio un problema' });
     }
 
-    if(modelo.img){
+    if (modelo.img) {
         const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
-        if(fs.existsSync(pathImagen)){
+        if (fs.existsSync(pathImagen)) {
             fs.unlinkSync(pathImagen)
         }
     }
-    
+
     const nombre = await subirArchivo(req.files, undefined, coleccion);
 
     modelo.img = nombre;
     await modelo.save();
 
-    res.json( modelo )
+    res.json(modelo)
 
 }
-
-
 
 const mostrarImagen = async (req = request, res = response) => {
 
@@ -94,20 +94,151 @@ const mostrarImagen = async (req = request, res = response) => {
             return res.status(500).json({ msg: 'ocurrio un problema' });
     }
 
-    if(modelo.img){
+    if (modelo.img) {
         const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
-        if(fs.existsSync(pathImagen)){
+        if (fs.existsSync(pathImagen)) {
             return res.sendFile(pathImagen)
         }
     }
 
     const pathImagen = path.join(__dirname, '../assets/no-image.jpg')
-    res.sendFile( pathImagen )
+    res.sendFile(pathImagen)
 
 }
 
 
+
+
+//###---------------  IMPLEMENTACION DE CLOUDINARY  ---------------###\\
+
+// se carga en cloudinary y devuelve el url pero no se guarda en ningun lado
+const cargarArchivoCloudinary = async (req = request, res = response) => {
+
+    try {
+
+        const { tempFilePath } = req.files.archivo;
+
+        console.log("tempFilePath");
+        console.log(tempFilePath);
+
+        //no esta aceptando tipo .xlsx
+        const { secure_url } = await cloudinary.uploader.upload(tempFilePath)
+
+        res.json({
+            img: secure_url
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({ msg:'Ocurrio un problema' })
+    }
+}
+
+
+
+const actualizarImagenCloudinary = async (req = request, res = response) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ msg: 'El usuario no existe' });
+            }
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ msg: 'El producto no existe' });
+            }
+            break;
+
+        default:
+            return res.status(500).json({ msg: 'ocurrio un problema' });
+    }
+
+    if (modelo.img) {
+
+        const auxArr = modelo.img.split('/');
+        const nombreAux = auxArr[auxArr.length - 1];
+        const [public_id] = nombreAux.split('.');
+
+        cloudinary.uploader.destroy(public_id);
+
+    }
+
+
+    const { tempFilePath } = req.files.archivo;
+
+    const { secure_url } = await cloudinary.uploader.upload(tempFilePath)
+
+    modelo.img = secure_url;
+    await modelo.save();
+
+    res.json(modelo)
+
+}
+
+
+const mostrarImagenCloudinary = async (req = request, res = response) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ msg: 'El usuario no existe' });
+            }
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ msg: 'El producto no existe' });
+            }
+            break;
+
+        default:
+            return res.status(500).json({ msg: 'ocurrio un problema' });
+    }
+
+    console.log(modelo);
+    if (modelo.img) {
+        //const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        //if(fs.existsSync(pathImagen)){
+        //    return res.sendFile(pathImagen)
+        //}
+
+        return res.json({
+            img: modelo.img
+        });
+    }
+
+    const pathImagen = path.join(__dirname, '../assets/no-image.jpg')
+    res.sendFile(pathImagen)
+
+}
+
+
+
+
+
+
 module.exports = {
+    cargarArchivoCloudinary,
+    actualizarImagenCloudinary,
+    mostrarImagenCloudinary,
+    
+    
     cargarArchivo,
     actualizarImagen,
     mostrarImagen,
--- routes/uploads.js
+++ routes/uploads.js
@@ -3,7 +3,7 @@ const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validarCampos');
 
-const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads');
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary, mostrarImagenCloudinary, cargarArchivoCloudinary } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
 const { validarArchivoSubir } = require('../middlewares');
 
@@ -11,7 +11,8 @@ const { validarArchivoSubir } = require('../middlewares');
 const router = Router();
 
 
-router.post( '/', validarArchivoSubir, cargarArchivo)
+//router.post( '/', validarArchivoSubir, cargarArchivo)
+router.post( '/', validarArchivoSubir, cargarArchivoCloudinary)
 
 router.put( '/:coleccion/:id', [
     validarArchivoSubir,
@@ -19,13 +20,15 @@ router.put( '/:coleccion/:id', [
     check('id', 'El id no es valido').isMongoId(),
     check('coleccion').custom( c => coleccionesPermitidas(c, ['usuarios','productos'])),
     validarCampos
-], actualizarImagen)
+], actualizarImagenCloudinary)
+//], actualizarImagen)
 
 router.get( '/:coleccion/:id', [
     check('id', 'El id es obligatorio').not().isEmpty(),
     check('id', 'El id no es valido').isMongoId(),
     check('coleccion').custom( c => coleccionesPermitidas(c, ['usuarios','productos'])),
     validarCampos
-], mostrarImagen)
+], mostrarImagenCloudinary)
+//], mostrarImagen)
 
 module.exports = router
